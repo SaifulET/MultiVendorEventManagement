c50ae66366a152e5b7bd7417e551e2e0648d0e33
SaifulET <si912999@gmail.com>
2026-02-01 01:38:40 +0600

    update

--- app/home/viewvenueprofile/[slug]/page.tsx
+++ app/home/viewvenueprofile/[slug]/page.tsx
@@ -1,9 +1,10 @@
 import VenueProviderPage from '@/app/component/home/viewvenueprofile/viewVenueprofile'
+import Navbar from '@/app/component/Pages/LeandingPage/Navbar'
 import React from 'react'
 
 function page() {
   return (
-    <div><VenueProviderPage/></div>
+    <div><Navbar></Navbar><VenueProviderPage/></div>
   )
 }
 
